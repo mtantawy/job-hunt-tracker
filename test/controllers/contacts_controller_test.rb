20e51272cda5f40639474f3049ffe4e69d43815b
Mahmoud Tantawy <me@mtantawy.com>
2023-06-04 20:42:59 +0200

    add simple integration tests for Contacts and ProcessSteps

--- test/controllers/contacts_controller_test.rb
+++ test/controllers/contacts_controller_test.rb
@@ -3,7 +3,39 @@
 require "test_helper"
 
 class ContactsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.create!(email: "example@example.com", password: "letmein")
+    @opportunity = Opportunity.create!(company_name: "Test Company", source_name: "name", source_link: "http://link")
+    sign_in
+  end
+
+  def sign_in
+    post(session_path(session: { email: "example@example.com", password: "letmein" }))
+  end
+
+  test "should create contact" do
+    assert_difference("Contact.count") do
+      post opportunity_contacts_path(@opportunity),
+        params: { contact: { name: "Test Contact", email: "test@example.com" } }
+    end
+
+    assert_redirected_to opportunity_path(@opportunity)
+  end
+
+  test "should update contact" do
+    contact = @opportunity.contacts.create!(name: "Test Contact")
+    patch opportunity_contact_path(@opportunity, contact),
+      params: { contact: { name: "New Contact Name" } }
+    assert_redirected_to opportunity_path(@opportunity)
+    assert_equal "New Contact Name", contact.reload.name
+  end
+
+  test "should destroy contact" do
+    contact = @opportunity.contacts.create!(name: "Test Contact")
+    assert_difference("Contact.count", -1) do
+      delete opportunity_contact_path(@opportunity, contact)
+    end
+
+    assert_redirected_to opportunity_path(@opportunity)
+  end
 end
--- test/controllers/process_steps_controller_test.rb
+++ test/controllers/process_steps_controller_test.rb
@@ -3,7 +3,39 @@
 require "test_helper"
 
 class ProcessStepsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.create!(email: "example@example.com", password: "letmein")
+    @opportunity = Opportunity.create!(company_name: "Test Company", source_name: "name", source_link: "http://link")
+    sign_in
+  end
+
+  def sign_in
+    post(session_path(session: { email: "example@example.com", password: "letmein" }))
+  end
+
+  test "should create process step" do
+    assert_difference("ProcessStep.count") do
+      post opportunity_process_steps_path(@opportunity),
+        params: { process_step: { title: "Test Process Step" } }
+    end
+
+    assert_redirected_to opportunity_path(@opportunity)
+  end
+
+  test "should update process step" do
+    process_step = @opportunity.process_steps.create!(title: "Test Process Step")
+    patch opportunity_process_step_path(@opportunity, process_step),
+      params: { process_step: { title: "New Process Step Title" } }
+    assert_redirected_to opportunity_path(@opportunity)
+    assert_equal "New Process Step Title", process_step.reload.title
+  end
+
+  test "should destroy process step" do
+    process_step = @opportunity.process_steps.create!(title: "Test Process Step")
+    assert_difference("ProcessStep.count", -1) do
+      delete opportunity_process_step_path(@opportunity, process_step)
+    end
+
+    assert_redirected_to opportunity_path(@opportunity)
+  end
 end
